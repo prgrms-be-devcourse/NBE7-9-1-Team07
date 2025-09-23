29896e0c9209077788bffd6e1d40763fac02bd37
SicpamaMinwoo <minwoo@sicpama.com>
2025-09-23 17:15:32 +0900

    Initialize project structure with backend and frontend folders

---
+++ backend/grids/src/main/java/com/grids/GridsApplication.java
@@ -0,0 +1,13 @@
+package com.grids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridsApplication.class, args);
+    }
+
+}
---
+++ backend/grids/src/test/java/com/grids/GridsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
