ad73ec234f7a330c269de788022bd6432e821d42
rwl <weilim0513@gmail.com>
2025-09-24 12:31:24 +0900

    BE/feat: OrderRepository에 userEmail 기준 최신 주문 조회 기능 추가

--- backend/grids/src/main/java/com/grids/domain/order/repository/OrderRepository.java
+++ backend/grids/src/main/java/com/grids/domain/order/repository/OrderRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    // userEmail을 기준으로 조회, 생성일 기준 내림차순으로 정렬
+    List<Order> findByUserEmailOrderByCreatedAtDesc(String userEmail);
 }
